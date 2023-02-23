d431a42cf5a9002a0b6e2b4ee3ac705ac7aa9986
Andrey Pestunov <andreypestunov@gmail.com>
2023-02-23 13:26:50 +0100

    chore: recover code base

---
+++ src/Net.Shared.Persistence.Models/Entities/Catalogs/PersistentCatalog.cs
@@ -0,0 +1,9 @@
+﻿namespace Net.Shared.Persistence.Models.Entities.Catalogs;
+
+public abstract class PersistentCatalog
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = null!;
+    public DateTime Created { get; init; } = DateTime.UtcNow;
+    public string? Description { get; init; }
+}
