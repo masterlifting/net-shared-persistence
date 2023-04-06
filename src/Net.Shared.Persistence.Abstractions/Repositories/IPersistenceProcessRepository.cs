15710aff727d695d4fe594deaaedfa29cc813a17
Andrey Pestunov <andreypestunov@gmail.com>
2023-04-06 19:58:50 +0200

    chore: recover code base

--- src/Net.Shared.Persistence.Abstractions/Repositories/IPersistenceProcessRepository.cs
+++ src/Net.Shared.Persistence.Abstractions/Repositories/IPersistenceProcessRepository.cs
@@ -5,7 +5,7 @@ namespace Net.Shared.Persistence.Abstractions.Repositories;
 
 public interface IPersistenceProcessRepository
 {
-    Task<Dictionary<string, T>> GetSteps<T>(CancellationToken cToken) where T : class, IPersistentProcessStep;
+    Task<T[]> GetSteps<T>(CancellationToken cToken) where T : class, IPersistentProcessStep;
     Task<T[]> GetProcessableData<T>(IPersistentProcessStep step, int limit, CancellationToken cToken) where T : class, IPersistentProcess;
     Task<T[]> GetUnprocessableData<T>(IPersistentProcessStep step, int limit, DateTime updateTime, int maxAttempts, CancellationToken cToken) where T : class, IPersistentProcess;
     Task SetProcessableData<T>(IPersistentProcessStep? step, IEnumerable<T> entities, CancellationToken cToken) where T : class, IPersistentProcess;
